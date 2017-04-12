beffd6a22e4bc28624e13f2da8392b5074f25c69
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2017-04-12 16:22:20 +0530

    clean up util; add data to hcn/

--- src/hcn/util.py
+++ src/hcn/util.py
@@ -1,27 +1,3 @@
-from enum import Enum
-
-
-EntType = Enum('Entity Type', '<party_size> <location> <cuisine> <rest_type> <non_ent>')
-
-party_sizes = ['1', '2', '3', '4', '5', '6', '7', '8', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
-locations = ['bangkok', 'beijing', 'bombay', 'hanoi', 'paris', 'rome', 'london', 'madrid', 'seoul', 'tokyo'] 
-cuisines = ['british', 'cantonese', 'french', 'indian', 'italian', 'japanese', 'korean', 'spanish', 'thai', 'vietnamese']
-rest_types = ['cheap', 'expensive', 'moderate']
-
-
-def ent_type(word):
-    if ent in party_sizes:
-        return EntType['party_size'].name
-    elif ent in locations:
-        return EntType['location'].name
-    elif ent in cuisines:
-        return EntType['cuisine'].name
-    elif ent in rest_types:
-        return EntType['rest_type'].name
-    else:
-        return word
-
-
 def read_content():
     return ' '.join([' '.join(row) for row in read_dialogs()])
 
@@ -41,12 +17,3 @@ def get_entities():
 
     with open('data/dialog-babi-kb-all.txt') as f:
         return filter_([item.split('\t')[-1] for item in f.read().split('\n') ])
-
-#def ent_extraction(utterance):
-#    return ' '.join(
-
-if __name__ == '__main__':
-    op = read_file()
-    print(op[:5])
-    #print(get_entities())
-
